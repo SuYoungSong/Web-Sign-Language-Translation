f3fc6a89d809cdbfb080df42aa3f6f6318606511
hokoro <hokoro123@gmail.com>
2023-12-06 14:24:29 +0900

    - 새로고침시 채팅 기록이 있으면 채팅 로그 가져오기

--- newchatgpt/views.py
+++ newchatgpt/views.py
@@ -19,17 +19,52 @@ import pandas as pd
 # has_ownership = [login_required, gpt_ownership_required]
 
 
+def chat_history(user):
+    user_record = ChatRecord.objects.filter(user=user)
+
+    group_data = user_record.values('pub_date', 'page').annotate(
+        question=F('question'),
+        datetime=F('pub_datetime'),
+    )
+
+    date = []
+    page = []
+    questions = []
+    answers = []
+    pub_date_times = []
+    for data in group_data:
+        date.append(data['pub_date'])
+        page.append(data['page'])
+        questions.append(data['question'])
+        pub_date_times.append(data['datetime'])
+
+    user_data = pd.DataFrame(
+        {'date': date, 'datetime': pub_date_times, 'page': page, 'question': questions})
+    test = user_data.sort_values(by='datetime', ascending=False).groupby(['date', 'page']).head(1)
+    dates = test['date'].unique()
+
+    result = []
+    for d in dates:
+        date_data = test.loc[test['date'] == d, :]
+        result.append({
+            'pub_date': d,
+            'message': [{'page': p, 'question': q} for p, q in
+                        zip(date_data['page'], date_data['question'])]
+        })
+
+    return result
+
+
 def index(request):
     is_exist = ChatUserCount.objects.filter(user=request.user).exists()  # 계정에서 GPT 사용한적이 있는지 여부
     is_refresh = request.headers.get('Cache-Control') == 'max-age=0'  # 새로고침 판단 여부
     result = dict()
     if is_refresh and is_exist:
         print('새로고침 상태 이기 떄문에 gpt 의 페이지가 증가하지 않습니다')
-        
+
         count = ChatUserCount.objects.get(user=request.user).count
-        print('conut:',count)
         page = ChatRecord.objects.filter(user=request.user).latest('page').page
-        print('page:',page)
+        result = chat_history(user=request.user)
         # 만약 새로고침을 했는데 ChatUserCount 에 있는 count 값이 Chatrecord 에 page 값에 있는 경우 -> 현재 카운트 페이지로 gpt 를 사용한적 있다 -> 이전 내용을
         # 불러온다
         if page == count:
@@ -44,41 +79,8 @@ def index(request):
             user = ChatUserCount.objects.get(user=request.user)
             user.count += 1
             user.save()
-            user_record = ChatRecord.objects.filter(user=request.user)
-
-            group_data = user_record.values('pub_date', 'page').annotate(
-                question=F('question'),
-                answer=F('answer'),
-                datetime=F('pub_datetime'),
-            )
-
-            date = []
-            page = []
-            questions = []
-            answers = []
-            pub_date_times = []
-            for data in group_data:
-                date.append(data['pub_date'])
-                page.append(data['page'])
-                questions.append(data['question'])
-                answers.append(data['answer'])
-                pub_date_times.append(data['datetime'])
-
-            user_data = pd.DataFrame(
-                {'date': date, 'datetime': pub_date_times, 'page': page, 'question': questions, 'answer': answers})
-            test = user_data.sort_values(by='datetime', ascending=False).groupby(['date', 'page']).head(1)
-            dates = test['date'].unique()
-
-            result = []
-            for d in dates:
-                date_data = test.loc[test['date'] == d, :]
-                result.append({
-                    'pub_date': d,
-                    'message': [{'page': p, 'question': q} for p, q in
-                                zip(date_data['page'], date_data['question'])]
-                })
-            print(result)
-            return render(request, 'newchatgpt/index.html', {'result': result})
+            result = chat_history(user=request.user)
+            # return render(request, 'newchatgpt/index.html', {'result': chat_history(user=request.user)})
         else:
             ChatUserCount.objects.create(user=request.user, count=1)
-    return render(request, 'newchatgpt/index.html')
+    return render(request, 'newchatgpt/index.html', {'result': result})
